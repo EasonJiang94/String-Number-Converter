acb87a9ceec4ae5aacab7c81d4006fb7e44617b0
EasonJiang <Eason.Jiang@beseye.com>
2023-09-10 00:30:26 -0400

    complete test-case generater

--- src/test.py
+++ src/test.py
@@ -1,42 +1,8 @@
-from str2num import solve
-
+from .str2num import solve
+from .text_def import *
 def number_to_string_3digit(number)->str:
     if number >= 1000:
         raise ValueError
-    ten_text = {
-        1: "ten", 
-        2: "twenty", 
-        3: "thirty", 
-        4: "forty", 
-        5: "fifty", 
-        6: "sixty", 
-        7: "seventy",  
-        8: "eighty", 
-        9: "ninety"        
-    }
-    teen_text = {
-        1: "eleven", 
-        2: "twelve", 
-        3: "thirteen",  
-        4: "fourteen", 
-        5: "fifteen", 
-        6: "sixteen", 
-        7: "seventeen", 
-        8: "eighteen", 
-        9: "nineteen"
-    }
-    single_digit_text = {
-        0: "zero", 
-        1: "one", 
-        2: "two", 
-        3: "three",  
-        4: "four", 
-        5: "five", 
-        6: "six", 
-        7: "seven", 
-        8: "eight", 
-        9: "nine"
-    }
     if number < 10:
         return single_digit_text[number]
     elif number < 100:
@@ -69,19 +35,43 @@ def number_to_string(num):
     For generating the test case
     '''
     # fake output 555555531
-    return "five hundred fifty-five million five hundred fifty-five thousand five hundred thirty-one"
-   
+    # return "five hundred fifty-five million five hundred fifty-five thousand five hundred thirty-one"
+    if num > 1e9:
+        print("Out of range, the input number should be less than 1e9")
+        raise ValueError
+    if num > 1e6: # one million        
+        remainder = int(num % 1e6)
+        quotient = int(num / 1e6)
+        if remainder == 0:
+            return "{} million".format(number_to_string_3digit(quotient))
+        else:
+            return "{} million {}".format(number_to_string_3digit(quotient), 
+                                          number_to_string(remainder))
+    elif num > 1e3:
+        remainder = int(num % 1e3)
+        quotient = int(num / 1e3)
+        if remainder == 0:
+            return "{} thousand".format(number_to_string_3digit(quotient))
+        else:
+            return "{} thousand {}".format(number_to_string_3digit(quotient), 
+                                          number_to_string(remainder))
+    else:
+        return number_to_string_3digit(num)
 
 
 
-if __name__ == "__main__":
+
+def main():
     # for test_num in range(1000000000):
-    for test_num in range(1000):
-        print(number_to_string_3digit(test_num))
     for test_num in range(1):
         test_num = 555555531 # just use fake input 
+        test_str = number_to_string(test_num)
+        print(test_str)
         ans_num = solve(test_num)
         if test_num == ans_num:
             print("Pass")
         else:
             print("Fail")
+
+if __name__ == "__main__":
+    main()
---
+++ src/text_def.py
@@ -0,0 +1,34 @@
+ten_text = {
+    1: "ten", 
+    2: "twenty", 
+    3: "thirty", 
+    4: "forty", 
+    5: "fifty", 
+    6: "sixty", 
+    7: "seventy",  
+    8: "eighty", 
+    9: "ninety"        
+}
+teen_text = {
+    1: "eleven", 
+    2: "twelve", 
+    3: "thirteen",  
+    4: "fourteen", 
+    5: "fifteen", 
+    6: "sixteen", 
+    7: "seventeen", 
+    8: "eighteen", 
+    9: "nineteen"
+}
+single_digit_text = {
+    0: "zero", 
+    1: "one", 
+    2: "two", 
+    3: "three",  
+    4: "four", 
+    5: "five", 
+    6: "six", 
+    7: "seven", 
+    8: "eight", 
+    9: "nine"
+}
